ee64b29874453cb8468bcc381aae68bfd9fdba43
ganesh <ganeshghorpade434@gmail.com>
2024-08-07 16:26:19 +0530

    config file  update

--- backend/database/config.js
+++ backend/database/config.js
@@ -1,2 +1,2 @@
 const { default: mongoose } = require('mongoose');
-mongoose.connect("mongodb://127.0.0.1:27017/Message")
+mongoose.connect("mongodb://127.0.0.1:27017/Message",{ useNewUrlParser: true, useUnifiedTopology: true });
